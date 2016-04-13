77605bc150b2dd893186683d545840fc94a91dc7
Cody Clawson <cclawson@student.neumont.edu>
2016-04-13 13:42:16 -0600

    added image to database and updated the controller

--- AmazonProducts.Domain/Product.cs
+++ AmazonProducts.Domain/Product.cs
@@ -18,6 +18,7 @@ namespace AmazonProducts.Domain
         public string Name { get; set; }
         public int CategoryID { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
     
         public virtual Category Category { get; set; }
     }
