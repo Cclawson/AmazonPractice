af5051c4d6bc7454b70695d5616af75fe747c9a1
Cody Clawson <cclawson@student.neumont.edu>
2016-04-16 20:38:45 -0600

    a

--- AmazonProducts.Domain/Category.cs
+++ AmazonProducts.Domain/Category.cs
@@ -21,6 +21,7 @@ namespace AmazonProducts.Domain
     
         public int Id { get; set; }
         public string CategoryName { get; set; }
+        public string Image { get; set; }
     
         public virtual ICollection<Product> Products { get; set; }
     }
